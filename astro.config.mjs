fdb8ce8a345ed59f9ebf9c48d0d921a592b57955
张天一 <zhangtianyi@r-213-99-25-172.comp.nus.edu.sg>
2026-01-16 19:13:32 +0800

    “update”

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://42x.online",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind()],
 });
-
-
